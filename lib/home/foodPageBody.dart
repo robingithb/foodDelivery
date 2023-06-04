91a22a6b28e841546e6195e5b434f48b078b5faf
robingithb <robinthomasq007@gmail.com>
2023-06-05 02:08:27 +0530

    there is some errors in buildpageitem and some properties to matrix need to be done

--- lib/home/foodPageBody.dart
+++ lib/home/foodPageBody.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fooddelivery/utils/colors.dart';
+import 'package:fooddelivery/widgets/big_text.dart';
+import 'package:fooddelivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -8,11 +11,15 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.red,
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -21,15 +28,67 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/images/food01.jpeg"))),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/food01.jpeg"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "chinees food"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5,
+                            (index) => Icon(
+                                  Icons.star,
+                                  color: AppColors.mainColor,
+                                  size: 15,
+                                )),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1281"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "comments")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
